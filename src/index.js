c22fcf72947e4307f65be94cc666ac87b990c463
Ravi Yadav <148179385+cipherravi@users.noreply.github.com>
2025-04-30 11:32:11 +0530

    add url in origin

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 const allowedOrigins = [
   "http://localhost:5173",
   "http://192.168.91.224:5173", // 👈 your local IP for mobile access
-  "https://foodie-backend-so1x.onrender.com", // (optional) for production
+  "https://cipherravi-foodie.vercel.app", // 👈 your production URL
 ];
 
 app.use(
