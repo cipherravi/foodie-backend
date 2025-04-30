5e55874c182848cbcf7f4e11019b8c02dbc45728
Ravi Yadav <148179385+cipherravi@users.noreply.github.com>
2025-04-30 11:04:34 +0530

    did some change in index.js

--- src/index.js
+++ src/index.js
@@ -45,5 +45,5 @@ app.get("/api/restaurantdata", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at ${PORT}`);
 });
