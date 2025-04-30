38e82b2b16cc2b1924aa943ec898687bcb69898c
Ravi Yadav <148179385+cipherravi@users.noreply.github.com>
2025-04-30 11:23:22 +0530

    made api for menu data

--- src/index.js
+++ src/index.js
@@ -43,6 +43,32 @@ app.get("/api/restaurantdata", async (req, res) => {
     res.status(500).json({ error: "Failed to fetch data" });
   }
 });
+app.get("/api/restaurantmenudata/:restaurantId", async (req, res) => {
+  try {
+    const { restaurantId } = req.params; // Get the restaurant ID from the params parameters
+
+    if (!restaurantId) {
+      return res.status(400).json({ error: "Restaurant ID is required" });
+    }
+    // Fetch the menu data for the specific restaurant
+    const response = await fetch(
+      `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.59430&lng=85.13520&restaurantId=${restaurantId}&catalog_qa=undefined&submitAction=ENTER`,
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
+          Accept: "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    res.json(data); // Send the actual JSON
+  } catch (error) {
+    // Handle errors
+    console.error("Error fetching Swiggy data:", error);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
